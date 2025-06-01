752276bdf31321ead14ef5330cc148c3c6692bfb
Andrii Batishchev <dvvp1998@gmail.com>
2025-06-02 00:03:24 +0300

    ДЗ 19.1. Cypress assertions, actions and custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,30 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+// cypress/support/commands.js
+Cypress.Commands.add('login', (email, password) => {
+    cy.visit('https://guest:welcome2qauto@qauto.forstudy.space/');
+    cy.get('.btn.btn-outline-white.header_signin').as('signInButton');
+    cy.get('@signInButton').click();
+    cy.get('#signinEmail').type(email);
+    cy.get('#signinPassword').type(password);
+    cy.get('.modal-footer .btn.btn-primary')
+        .should('not.be.disabled')
+        .click();
+});
+
+Cypress.Commands.overwrite('type', (originalFn, element, text, options) => {
+  if (options && options.sensitive) {
+    // turn off original log
+    options.log = false
+    // create our own log with masked message
+    Cypress.log({
+      $el: element,
+      name: 'type',
+      message: '*'.repeat(text.length),
+    })
+  }
+
+  return originalFn(element, text, options)
+})
